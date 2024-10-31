b4d98b703de31807f5087b03a82051e95363ba9f
Buddha Christ <budchris@solrey3.com>
2024-10-31 13:56:47 -0400

    add kickstart.nvim and lsp to nvim config

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -614,31 +614,51 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
-        pyright = {},
-        tsserver = {},
-        html = {},
-        cssls = {},
-        bashls = {},
-        jsonls = {},
-        yamlls = {},
-        sqls = {},
-        terraformls = {},
-        dockerls = {},
-        nil_ls = {},
+        -- clangd = {},
+        -- gopls = {},
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
+        --
+
         lua_ls = {
+          -- cmd = {...},
+          -- filetypes = { ...},
+          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              diagnostics = {
-                globals = { 'vim' },
-              },
+              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
-        intelephense = {},
+        -- More LSPs can be added here
+        bashls = {},
+        clangd = {},
+        cssls = {},
+        dockerls = {},
+        html = {},
+        htmx = {},
+        jsonls = {},
+        marksman = {},
+        phpactor = {},
+        postgresql = {},
+        pyright = {},
+        rnix = {},
+        sqls = {},
+        terraformls = {},
+        tsserver = {},
+        vimls = {},
+        yamlls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -895,9 +915,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-        ensure_installed = {
-          'bash', 'c', 'css', 'dockerfile', 'html', 'javascript', 'json', 'lua', 'markdown', 'nix', 'php', 'python', 'sql', 'terraform', 'typescript', 'vim', 'yaml', 'zsh'
-        },
+      ensure_installed = { 'bash', 'c', 'css', 'diff', 'dockerfile', 'hcl', 'helm', 'html', 'javascript', 'jq', 'json', 'just', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'nginx', 'nix', 'php', 'python', 'query', 'regex', 'sql', 'toml', 'typescript', 'vim', 'vimdoc', 'yaml', 'zsh' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
